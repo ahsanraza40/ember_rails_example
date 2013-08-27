64d0d0a39e90d3245c3b53e8917f0cece5f5d5e9
Hiroaki Nakamura <hnakamur@gmail.com>
2013-08-27 15:44:41 +0900

    Add initial app source files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberRailsExample::Application.config.secret_token = '433808bef97eb0c5592fb5d78c94a632eb261cd9a91a66cf4870c425db38b4727f271452043d20d6305d6c668e108ba1a516a1b10f2c8e9811ff618431e10508'
