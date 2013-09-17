bd2ec11dbfc73d909858cd19b72c1a2b1fede0aa
Hiroaki Nakamura <hnakamur@gmail.com>
2013-09-17 15:40:47 +0900

    Modify views for cover_image.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827065321) do
+ActiveRecord::Schema.define(version: 20130917060645) do
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.date     "released_on"
+    t.string   "cover_image"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "todos", force: true do |t|
     t.string   "content"
